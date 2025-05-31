0bae5aef23fffe507a9f82ad3762a1d91bb34f1f
Kyriaki Forfoli <kforfoli@c3a1c5.42berlin.de>
2025-05-31 19:24:25 +0200

    pushiiiit

--- src/player.c
+++ src/player.c
@@ -6,7 +6,7 @@
 /*   By: kforfoli <kforfoli@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/24 23:39:16 by wel-safa          #+#    #+#             */
-/*   Updated: 2025/05/31 18:30:58 by wel-safa         ###   ########.fr       */
+/*   Updated: 2025/05/31 19:23:24 by kforfoli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 void	ns(t_game *g)
 {
-	if (g->conf->player == 'S')
+	if (g->conf->player == 'N')
 	{
-		g->player_dir_x = 1;
-		g->player_dir_y = 0;
+		g->player_dir_x = 0;
+		g->player_dir_y = 1;
 		g->plane_x = -0.66;
 		g->plane_y = 0.0;
 	}
-	else if (g->conf->player == 'N')
+	else if (g->conf->player == 'S')
 	{
 		g->player_dir_x = 0;
 		g->player_dir_y = -1;
